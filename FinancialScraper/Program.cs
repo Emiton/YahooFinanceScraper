814307b8712e58d8cad6b240c0bacf31242af4b3
Emiton <emiton_alves@my.uri.edu>
2018-06-28 15:05:50 -0400

    Initial commit

---
+++ FinancialScraper/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace FinancialScraper
+{
+    class YahooScraper
+    {
+        static void Main(string[] args)
+        {
+            // Create a new chrome driver
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                // Go to main page
+                driver.Navigate().GoToUrl("https://finance.yahoo.com");
+
+                // *A* Click the link to 'My Portfolio'
+                driver.FindElement(By.XPath("//a[@href='/portfolios?bypass=true']")).Click();
+
+                // *B* Click the sign in button
+                driver.FindElement(By.XPath("//*[@id='uh']/header/section/div[2]/a")).Click();
+
+                // Reverse A and B
+            }
+        }
+    }
+}
