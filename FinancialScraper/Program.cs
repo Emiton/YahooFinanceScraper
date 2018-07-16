9dd8f705e5b23158b85de8348bd0143428684752
Emiton <emiton_alves@my.uri.edu>
2018-07-16 12:38:18 -0400

    Now able to login to yahoo finance

--- FinancialScraper/Program.cs
+++ FinancialScraper/Program.cs
@@ -19,12 +19,46 @@ namespace FinancialScraper
                 // Go to main page
                 driver.Navigate().GoToUrl("https://finance.yahoo.com");
 
+                // Click the sign in button
+                driver.FindElement(By.Id("uh-signedin")).Click();
+
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+                // Find username login
+                IWebElement email = driver.FindElement(By.Id("login-username"));
+                email.SendKeys("YOUR_EMAIL");
+                try
+                {
+                    driver.FindElement(By.XPath("//*[@id=\"login-signin\"]")).Click();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("IT WAS NEVER CLICKED!");
+                }
+
+                Console.WriteLine("I GUESS WE CLICKED SOMETHING!");
+
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+                // Find password input
+                IWebElement password = driver.FindElement(By.Id("login-passwd"));
+
+                password.SendKeys("YOUR_PASSWORD");
+                driver.FindElement(By.Id("login-signin")).Click();
+
+
+
                 // *A* Click the link to 'My Portfolio'
-                driver.FindElement(By.XPath("//a[@href='/portfolios?bypass=true']")).Click();
+                //driver.FindElement(By.XPath("//a[@href='/portfolios?bypass=true']")).Click();
 
-                // *B* Click the sign in button
-                driver.FindElement(By.XPath("//*[@id='uh']/header/section/div[2]/a")).Click();
-
+                //try
+                //{
+                //    // *B* Click the sign in button
+                //    driver.FindElement(By.XPath("//*[@id='uh']/header/section/div[2]/a")).Click();
+                //}
+                //catch (Exception e)
+                //{
+                //    Console.WriteLine("IT NEVER FOUND THE SIGN IN BUTTON");
+                //}
                 // Reverse A and B
             }
         }
